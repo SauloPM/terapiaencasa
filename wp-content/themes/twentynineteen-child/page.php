ee0ea6ef8f42b96bc85b474a68c0e420367ce39f
Saulo Poveda Montesdeoca <saulo.poveda@dxc.com>
2020-05-09 14:55:19 +0100

    [ Inicio ] » Contenido actualizado. Estilos actualizados

--- wp-content/themes/twentynineteen-child/page.php
+++ wp-content/themes/twentynineteen-child/page.php
@@ -50,11 +50,11 @@ $menuItems = wp_get_nav_menu_items( $menu->term_id, array( 'order' => 'DESC' ) )
         </div>
     </header>
 
-    <!-- ────────────────── -->
-    <!--     BIENVENIDA     -->
-    <!-- ────────────────── -->
+    <!-- ─────────────────── -->
+    <!--     INFORMACIÓN     -->
+    <!-- ─────────────────── -->
 
-    <section id="bienvenida">
+    <section>
 
         <h2 class="titulo-seccion">Quiénes somos</h2>
         <hr>
@@ -73,13 +73,16 @@ $menuItems = wp_get_nav_menu_items( $menu->term_id, array( 'order' => 'DESC' ) )
     <!--     CALLOUT     -->
     <!-- ─────────────── -->
 
-    <aside class="callout">
-        <span class="texto">
-            Atención online a <span class="texto subrayado">toda</span> la comunidad hispanohablante
-        </span>
+    <aside class="callout bg-callout-1">
+        <span class="texto"><span class="texto tilde">Atencion</span> online a toda
+la comunidad hispanohablante</span>
     </aside>
 
-    <section id="tratamiento">
+    <!-- ─────────────────── -->
+    <!--     INFORMACIÓN     -->
+    <!-- ─────────────────── -->
+
+    <section>
 
         <h2 class="titulo-seccion">Qué abarcamos</h2>
 
@@ -87,20 +90,66 @@ $menuItems = wp_get_nav_menu_items( $menu->term_id, array( 'order' => 'DESC' ) )
             En Terapia en casa trabajamos basándonos en la orientación cognitivo-conductal. En nuestras sesiones de psicología online tratamos problemas de miedo, ansiedad, depresión, hipocondría, autoestima, autorrealización personal, etc. Además, podemos abordar otros problemas más profundos como trastornos de conducta, problemas de agresividad o de autocontrol, adicciones y dificultades de relación. También te brindamos la oportunidad de poder realizar sesiones de psicología online para más de una persona y tratar problemas de pareja y problemas familiares al mismo precio, sin ningún tipo de incremento.
         </p>
 
-        <a href="" class="boton">Ampliar información</a>
-
-        <?php
-
-            // while ( have_posts() ) :
-
-            //     the_post();
-
-            //     get_template_part( 'template-parts/content/content', 'page' );
-
-            // endwhile;
-        ?>
+        <a href="#" class="boton">Ampliar información sobre estos y otros servicios</a>
 
     </section>
+
+    <!-- ─────────────── -->
+    <!--     CALLOUT     -->
+    <!-- ─────────────── -->
+
+    <aside class="callout bg-callout-2">
+        <span class="texto">La primera <span class="texto tilde">sesion</span>
+de terapia online es gratuita</span>
+    </aside>
+
+    <!-- ─────────────────── -->
+    <!--     INFORMACIÓN     -->
+    <!-- ─────────────────── -->
+
+    <section>
+
+        <h2 class="titulo-seccion">Ventajas de la terapia online</h2>
+
+        <p class="">
+            Trato personal, cercano y honesto. La primera sesión es GRATIS. Ahorro de tiempo y dinero. Sin barreras geográficas, desde la comodidad e intimidad de tu casa. 
+        </p>
+
+        <h2 class="titulo-seccion">¿Cómo lo hacemos?</h2>
+
+        <p class="margin-bottom-35">
+            Es muy sencillo. Nuestras sesiones de psicología online se realizan a través de una videoconferencia vía Skype u otro medio digital y seguro que nos quieras proponer. Solamente necesitarás tener un ordenador, móvil o tablet que disponga de cámara y conexión a internet. Tras recibir tu solicitud, estudiar tu situación y comprobar que cumples con los requisitos, concertaremos una cita contigo con el miembro de nuestro equipo de psicólogos que más se adapte a tus necesidades.
+        </p>
+
+        <a href="#" class="boton">Ampliar información sobre psicoterapia online</a>
+
+    </section>
+
+    <!-- ─────────────── -->
+    <!--     CALLOUT     -->
+    <!-- ─────────────── -->
+
+    <aside class="callout bg-callout-3">
+        <span class="texto"><span class="texto tilde">Atencion</span> online a toda
+la comunidad hispanohablante</span>
+    </aside>
+
+    <!-- ─────────────────── -->
+    <!--     INFORMACIÓN     -->
+    <!-- ─────────────────── -->
+
+    <section>
+
+        <h2 class="titulo-seccion">Pasos a seguir</h2>
+
+        <p class="margin-bottom-35">
+            Contacta con nosotros sin compromiso y dinos en qué te podemos ayudarte. Te orientaremos en la situación en la que te encuentres y concertaremos una primera cita gratuita. El psicólogo asignado acordará contigo una cita para tu sesión de psicoterapia online. Puedes elegir al profesional que tú quieras si tienes alguna preferencia personal. Este valorará tu situación y en caso de ser necesaria la continuación del proceso terapéutico, te explicará las distintas formas de pago y los tiempos para realizarlos. El día y hora señalados nos conectaremos contigo de forma puntual y procederemos a realizar tu terapia psicológica.
+        </p>
+
+        <a href="#" class="boton">Recuerda que la primera sesión es gratuita</a>
+
+    </section>
+
 </div>
 
 <!-- ────────────── -->
