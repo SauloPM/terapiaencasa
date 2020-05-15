563d0451c84d8f5730d873cd7eec8849262c2bb5
Saulo Poveda Montesdeoca <saulo.poveda@dxc.com>
2020-05-15 12:41:00 +0100

    [ Widgets ] » Widgets que vienen por defecto en el dashboard de WordPress desactivados

--- wp-content/themes/terapia-en-casa/functions.php
+++ wp-content/themes/terapia-en-casa/functions.php
@@ -20,9 +20,10 @@ register_nav_menus(
 //     WIDGETS     //
 // ─────────────── //
 
+// Crear áreas de widgets
 function crear_areas_de_widgets() {
 
-    // Área de widgets 1
+    // Área de widgets 1 (Portada)
     register_sidebar(
         array (
             'id'            => 'portada',
@@ -36,8 +37,34 @@ function crear_areas_de_widgets() {
     );
 }
 
-// Habilitar sección en el dashboard (Apariencia / Menú)
 add_action( 'widgets_init', 'crear_areas_de_widgets' );
+
+// Desactivar los widgets de WordPress del dashboard
+function desactivar_widgets_de_wordpress() {
+
+    unregister_widget('WP_Widget_Pages');
+    unregister_widget('WP_Widget_Calendar');
+    unregister_widget('WP_Widget_Archives');
+    unregister_widget('WP_Widget_Links');
+    unregister_widget('WP_Widget_Meta');
+    unregister_widget('WP_Widget_Search');
+    unregister_widget('WP_Widget_Text');
+    unregister_widget('WP_Widget_Media_Image');
+    unregister_widget('WP_Widget_Categories');
+    unregister_widget('WP_Widget_Recent_Posts');
+    unregister_widget('WP_Widget_Recent_Comments');
+    unregister_widget('WP_Widget_RSS');
+    unregister_widget('WP_Widget_Tag_Cloud');
+    unregister_widget('WP_Nav_Menu_Widget');
+    unregister_widget('Twenty_Eleven_Ephemera_Widget');
+    unregister_widget('WP_Widget_Custom_HTML');
+    unregister_widget('WP_Widget_Media_Audio');
+    unregister_widget('WP_Widget_Media_Image');
+    unregister_widget('WP_Widget_Media_Video');
+    unregister_widget('WP_Widget_Media_Gallery');
+}
+
+add_action( 'widgets_init', 'desactivar_widgets_de_wordpress', 11 );
 
 // ─────────── //
 //     CSS     //
