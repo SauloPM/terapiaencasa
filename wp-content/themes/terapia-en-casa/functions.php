8c0ce6f85fcccb175dc49680f3a522e8a276a888
Saulo Poveda Montesdeoca <saulo.poveda@dxc.com>
2020-05-16 14:57:54 +0100

    [ General ] » Las fuentes de Goolge incluidas vía CDN desde la hoja de estilos de la theme se incluyen ahora vía PHP en el fichero 'functions.php'

--- wp-content/themes/terapia-en-casa/functions.php
+++ wp-content/themes/terapia-en-casa/functions.php
@@ -75,6 +75,14 @@ function cargar_fuentes() {
     // Indie Flower
     wp_register_style( 'IndieFlower', 'https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap' );
     wp_enqueue_style( 'IndieFlower' );
+
+    // Amatic SC
+    wp_register_style( 'AmaticSC', 'https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap' );
+    wp_enqueue_style( 'AmaticSC' );
+
+    // Amatic SC
+    wp_register_style( 'Roboto', 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap' );
+    wp_enqueue_style( 'Roboto' );
 }
 
 add_action( 'wp_enqueue_scripts', 'cargar_fuentes' );
