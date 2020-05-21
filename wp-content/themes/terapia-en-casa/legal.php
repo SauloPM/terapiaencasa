219da173346ab0f57a343c5408eaf91dca9f62c4
Saulo Poveda Montesdeoca <saulo.poveda@dxc.com>
2020-05-21 15:05:44 +0100

    [ Legal ] » Política de privacidad y de cookies preparadas

---
+++ wp-content/themes/terapia-en-casa/legal.php
@@ -0,0 +1,18 @@
+<?php /* Template Name: Página de texto legal */ ?>
+
+<?php get_header(); ?>
+
+<section id="legal">
+
+    <h2 class="titulo-seccion"><?php the_title(); ?></h2>
+
+    <?php if ( have_posts() ) : ?>
+        <?php while ( have_posts() ) : ?>
+            <?php the_post(); ?>
+            <?php the_content(); ?>
+        <?php endwhile; ?>
+    <?php endif; ?>
+
+</section>
+
+<?php get_footer(); ?>
