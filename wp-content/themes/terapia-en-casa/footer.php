0212460ff3bdb4b421cd9dcfe74822b0fb046d16
Saulo Poveda Montesdeoca <saulo.poveda@dxc.com>
2020-05-21 15:40:40 +0100

    [ Fichero 'footer.php' ] » Menú de navegación del pie de página incluido. Estilos actualizados

--- wp-content/themes/terapia-en-casa/footer.php
+++ wp-content/themes/terapia-en-casa/footer.php
@@ -3,29 +3,22 @@
         <!--     FOOTER     -->
         <!-- ────────────── -->
 
-        <section id="contacto">
-
-            <h2 class="titulo-seccion">Contacto</h2>
-
-            <?php echo do_shortcode( '[contact-form-7 id="84" title="Formulario de contacto"]' ); ?>
-
-        </section>
-
         <footer id="footer">
 
-            <!-- Enlaces a RRSS -->
-            <?php dynamic_sidebar( 'pie-de-pagina' ); ?>
+            <!-- Copyright -->
+            <div class="copyright">&copy; Terapia en casa <?php echo date("Y"); ?></div>
 
-            <!-- Legal -->
+            <!-- Menú de navegación -->
+            <?php
+                wp_nav_menu(
 
-            <div class="legal">
-                <div class="copyright">&copy; Terapia en casa <?php echo date("Y"); ?></div>
-                <div>
-                    <a href="" class="enlace">Política de privacidad</a>
-                    <a href="" class="enlace">Política de cookies</a>
-                </div>
-            </div>
-
+                    array(
+                        'theme_location' => 'footer-menu',
+                        'menu_class' => 'legal',
+                        'container' => '',
+                    )
+                );
+            ?>
         </footer>
     </div>
 
