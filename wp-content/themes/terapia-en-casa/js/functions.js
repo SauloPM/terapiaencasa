1848bb81b1b587931baa01fda85a192744605e12
Saulo Poveda Montesdeoca <saulo.poveda@dxc.com>
2020-05-22 13:23:48 +0100

    [ Fichero 'functions.js' ] » La navegación suavizada se activa también con los enlaces de saber más. Validaciones del formulario de contacto incluidas

--- wp-content/themes/terapia-en-casa/js/functions.js
+++ wp-content/themes/terapia-en-casa/js/functions.js
@@ -1,5 +1,12 @@
 $( window ).on( 'load', function() {
 
+    $('#play-video').on('click', function(ev) {
+
+        $("#video")[0].src += "&autoplay=1";
+        ev.preventDefault();
+        
+    });
+
     // ─────────────── //
     //     SPINNER     //
     // ─────────────── //
@@ -23,7 +30,7 @@ $( window ).on( 'load', function() {
     //     NAVEGACIÓN     //
 	// ────────────────── //
 
-    $( document ).on( 'click', '.menu-item a', function( evento ) {
+    $( document ).on( 'click', '.menu-item a, .saber-mas a', function( evento ) {
         
         evento.preventDefault();
 
@@ -46,6 +53,42 @@ $( window ).on( 'load', function() {
 
     // Evitar el autocompletado
     $( '.entrada input' ).attr( 'autocomplete', 'off' );
+
+    // Activar o desactivar botón
+    $( document ).on( 'change', '.lopd input', function() {
+
+        var boton = $( '#contacto .boton' );
+
+        estadoActual = ( boton.hasClass( 'activado' )) ? 'activado' : 'desactivado';
+        estadoPrevio = ( estadoActual == 'activado' )  ? 'desactivado' : 'activado';
+
+        boton.removeClass( estadoActual ).addClass( estadoPrevio );
+    });
+
+    // No hacer nada al hacer clic en el botón si el usuario no ha aceptado la política de privacidad
+    $( document ).on( 'click', '#contacto .boton', function( evento ) {
+
+        var boton = $( this );
+
+        if ( boton.hasClass( 'desactivado' ) ) {
+
+            evento.preventDefault();
+            
+        } else {
+
+            
+        }
+    });
+
+    // Ocultar notificación
+    // $( document ).on( 'click', '.wpcf7-response-output', function() {
+
+    //     $( this ).css( 'opacity', '0' );
+
+    //     setTimeout(() => {
+    //         $( this ).css( 'display', 'none' );
+    //     }, 500);
+    // });
 
 	// ─────────────────── //
     //     TESTIMONIOS     //
