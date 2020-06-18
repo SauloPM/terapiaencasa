ad50acd6de0afd77c8b2f21b689cf409dbb73e8a
Saulo Poveda Montesdeoca <saulo.poveda@dxc.com>
2020-06-18 02:30:19 +0100

    [ Fichero 'single.php' ] » Vista post terminada

---
+++ wp-content/themes/terapia-en-casa/single.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+
+<?php
+
+    if ( have_posts() ) {
+
+        echo "<div class='post wp-block-group'>";
+
+        while ( have_posts() ) {
+            
+            the_post();
+
+            the_title( "<h2>", "</h2>" );
+
+            if ( has_post_thumbnail() ) {
+                the_post_thumbnail( "full", array( "class" => "imagen-destacada" ));
+            }
+
+            echo "<p>" . the_content() . "</p>";
+
+            // echo "<div class='fecha'>" . get_the_date() . "</div>";
+
+        }
+
+        echo "</div>";
+    }
+
+?>
+
+<?php get_footer(); ?>
