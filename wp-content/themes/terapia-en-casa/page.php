170651fa71036a12ded8596c86c32702621091ea
Saulo Poveda Montesdeoca <saulo.poveda@dxc.com>
2020-06-30 19:12:53 +0100

    [ Fichero 'page.php' ] » Formulario de contacto incluido

--- wp-content/themes/terapia-en-casa/page.php
+++ wp-content/themes/terapia-en-casa/page.php
@@ -1,10 +1,18 @@
 <?php get_header(); ?>
 
+<h2><?php the_title(); ?></h2>
+
 <?php if ( have_posts() ) : ?>
     <?php while ( have_posts() ) : ?>
         <?php the_post(); ?>
         <?php the_content(); ?>
     <?php endwhile; ?>
 <?php endif; ?>
+
+<!-- ──────────────── -->
+<!--     CONTACTO     -->
+<!-- ──────────────── -->
+
+<?php include 'shared/formulario-contacto.php' ?>
 
 <?php get_footer(); ?>
