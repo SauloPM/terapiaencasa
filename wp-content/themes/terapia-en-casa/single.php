3b90b2f2b7767c2b7d8ebf014c555ced106aca1c
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2021-01-28 21:40:19 +0000

    [ Post ] » Incluir fotografía y el nombre del autor completado

--- wp-content/themes/terapia-en-casa/single.php
+++ wp-content/themes/terapia-en-casa/single.php
@@ -11,9 +11,24 @@
             the_post();
 
             the_title( "<h2>", "</h2>" );
-
+            
             if ( has_post_thumbnail() ) {
                 the_post_thumbnail( "full", array( "class" => "imagen-destacada" ));
+            }
+
+            // Almacenamos en una variable los campos personalizados del post
+            $campos = get_fields( get_the_ID() );
+
+            if ( strlen( trim( $campos["fotografia"]["url"] )) > 0 ) {
+
+                // Imagen
+                echo "<div class='fotografia' style='background-image: url(" . $campos["fotografia"]["url"] . ")'></div>";
+            }
+            
+            if ( strlen( trim( $campos[ "autor" ] )) > 0 ) {
+                
+                // Autor
+                echo "<div class='autor'>" . "Por ". $campos[ "autor" ] . ", ". $campos["puesto"] . "</div>";
             }
 
             echo the_content();
