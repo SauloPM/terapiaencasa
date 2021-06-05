095c22d5ff5cf78e6bcec54029bd3857fb105d64
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2021-06-05 16:46:50 +0100

    [ SEO ] » Crear página de error 404 personalizada completado

---
+++ wp-content/themes/terapia-en-casa/404.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<div class="wp-block-group">
+    <div class="wp-block-group__inner-container">
+
+        <h2>Página no encontrada</h2>
+
+        <p>
+            La página a la que intentas acceder no existe. No te preocupes, puedes volver a la página de inicio haciendo clic en el enlace que aparece a continuación. También puedes utilizar el menú de navegación si deseas acceder a cualquier otra sección de Terapia en casa.
+        </p>
+
+        <p class="saber-mas">
+            <a href="https://terapiaencasa.es/">Volver a la página de inicio</a>
+        </p>
+
+    </div>
+</div>
+
+<?php get_footer(); ?>
