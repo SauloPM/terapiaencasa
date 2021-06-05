06baeefc75aed99707c79ecb9632f5cd3dd90f51
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2021-06-05 15:12:59 +0100

    [ Testimonios ] » Refactorizar código para que no aparezcan los warnings completado

--- wp-content/themes/terapia-en-casa/shared/testimonios.php
+++ wp-content/themes/terapia-en-casa/shared/testimonios.php
@@ -1,44 +1,36 @@
-<?php
-
-    $items = get_posts(
-
-        array(
-            'post_per_page' => -1,
-            'post_type' => 'testimonios',
-            'category_name' => $type
-        )
-    );
-
-    $catObj = get_category_by_slug( $type );
-    $parent = get_the_category_by_ID( $catObj->category_parent );
-?>
+<?php $items = get_posts( array( 'post_type' => 'testimonios', 'numberposts'=> -1 )); ?>
 
 <?php if ( $items ): ?>
 
-<aside class="testimonios">
+    <aside class="testimonios">
 
-    <div class="carrusel owl-carousel owl-theme owl-height">
-        <?php foreach ( $items as $item ) : ?>
-        <div class="item">
+        <div class="carrusel owl-carousel owl-theme owl-height">
 
-            <?php
-                
-            $campos = get_fields( $item->ID );
+            <?php foreach ( $items as $item ) : ?>
 
-            setup_postdata( $item );
-            echo $campos["testimonio"];
-            wp_reset_postdata();
+                <div class="item">
 
-            ?>
+                    <?php
+                        
+                    $campos = get_fields( $item->ID );
+
+                    setup_postdata( $item );
+                    echo $campos["testimonio"];
+                    wp_reset_postdata();
+
+                    ?>
+
+                </div>
+
+            <?php endforeach; ?>
 
         </div>
-        <?php endforeach; ?>
-    </div>
 
-    <div class="flechas">
-        <i class="flecha flecha-derecha fa fa-angle-right"></i>
-        <i class="flecha flecha-izquierda fa fa-angle-left"></i>
-    </div>
-</aside>
+        <div class="flechas">
+            <i class="flecha flecha-derecha fa fa-angle-right"></i>
+            <i class="flecha flecha-izquierda fa fa-angle-left"></i>
+        </div>
+        
+    </aside>
 
 <?php endif; ?>
